2fe42260fdc6200f25f3b9a4fec11144a3bf6771
Omorval3008 <omorval3008@g.educaand.es>
2023-12-14 10:54:51 +0100

    inicio proyecto

---
+++ src/main/java/daw/Ej1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author oscar
+ */
+public class Ej1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public static int numPalabras() {
+        boolean errorOpcion=true;
+        int numPalabras=0;
+        do{
+            try {
+                numPalabras = Integer.parseInt(JOptionPane.showInputDialog("Introduce "
+                        + "el número de palabras que desea crear"));
+                errorOpcion = false;
+            } catch (NumberFormatException nfe) {
+                JOptionPane.showMessageDialog(null, "Has introducido un "
+                        + "valor no númerico");
+                errorOpcion = true;
+            }
+        }while(errorOpcion);
+        return numPalabras;
+    }
+}
+
